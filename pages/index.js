81b0395ee67685e7de69864179696be51e5c0a4c
Pham Hoang Viet <42289889+HoangVietPham2000@users.noreply.github.com>
2019-11-16 14:36:56 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default class IndexPage extends React.PureComponent {
         <iframe
           width="0"
           height="0"
-          src="https://www.youtube.com/embed/gphA89W5xdQ?autoplay=1&amp;rel=0&amp;controls=0&amp;showinfo=0"
+          src="https://www.youtube.com/embed/-Bnj1-I1Q6c?autoplay=1&amp;rel=0&amp;controls=0&amp;showinfo=0"
           frameBorder="0"
           allow=""
           allowFullScreen
